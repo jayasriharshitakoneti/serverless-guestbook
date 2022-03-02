587f55b6f071f16a9105dfeee9cafaeae835d264
Jaya Sriharshita Koneti <75335955+jayasriharshitakoneti@users.noreply.github.com>
2022-03-03 00:13:05 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://fc07a08b.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
